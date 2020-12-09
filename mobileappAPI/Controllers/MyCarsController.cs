afdd51fbec7a0823026d302f3837342b62049cbd
Filfeni <47361476+Filfeni@users.noreply.github.com>
2020-12-08 20:54:53 -0400

    Added Reservation Controller

--- mobileappAPI/Controllers/MyCarsController.cs
+++ mobileappAPI/Controllers/MyCarsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,14 +15,14 @@ namespace mobileappAPI.Controllers
     [Authorize]
     public class MyCarsController : ControllerBase
     {
-        /*  /mycars		        GET
-            /mycars/	        POST
-            /mycars/:id	        PATCH
-            /mycars/:id	        DELETE
-            /mycars/:id/post    GET
-            /mycars/:id/post    POST
-            /mycars/:id/post    PUT
-            /mycars/:id/post    DELETE
+        /*  /mycars		        GET *
+            /mycars/	        POST *
+            /mycars/:id	        PUT *
+            /mycars/:id	        DELETE *
+            /mycars/:id/post    GET *
+            /mycars/:id/post    POST *
+            /mycars/:id/post    PUT *
+            /mycars/:id/post    DELETE *
         */
         private readonly AuthContext _context;
         public MyCarsController(AuthContext context)
@@ -38,21 +39,20 @@ namespace mobileappAPI.Controllers
             {
                 return NotFound();
             }
-            return myCars;        
+            return myCars;
         }
 
         [HttpPost]
-        public async Task<ObjectResult> RegisterCar(Carro car)
+        public async Task<ActionResult> RegisterCar(Carro car)
         {
             try
             {
                 _context.Carros.Add(car);
                 await _context.SaveChangesAsync();
-                
+
             }
             catch (System.Exception)
             {
-
                 return BadRequest(new Response() { Status = "Error", Message = "This car could not be created, please try again" });
             }
             return Ok(new Response() { Status = "Success", Message = "This car was registered!" });
@@ -61,29 +61,25 @@ namespace mobileappAPI.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Carro>> GetMyCarsSpecific(int id)
         {
-            var carro = await _context.Carros.FindAsync(id);
+            int userid = _context.Users.Single(u => u.UserName == User.Identity.Name).Usuario.Idusuario;
+            var myCar = await _context.Carros.SingleAsync(x => x.Idpropietario == userid && x.Idcarro == id);
 
-            if (carro == null)
+            if (myCar == null)
             {
                 return NotFound();
             }
 
-            return carro;
+            return myCar;
         }
-        
+
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateMyCar(int id, Carro car)
         {
             int userid = _context.Users.Single(u => u.UserName == User.Identity.Name).Usuario.Idusuario;
-            if (userid != car.Idpropietario)
+            if (userid != car.Idpropietario || id != car.Idcarro)
             {
                 return BadRequest();
             }
-            if (id != car.Idcarro)
-            {
-                return BadRequest();
-            }
-
             _context.Entry(car).State = EntityState.Modified;
             try
             {
@@ -101,26 +97,21 @@ namespace mobileappAPI.Controllers
                 }
             }
 
-            return Ok( new Response(){Status = "Success", Message = "This car was updated!"});
+            return Ok(new Response() { Status = "Success", Message = "This car was updated!" });
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteMyCar(int id)
         {
             if (!MyCarExists(id))
-            {
                 return NotFound();
-            }
 
-            var car = await _context.Carros.FindAsync();
+            var car = await _context.Carros.FindAsync(id);
             int userid = _context.Users.Single(u => u.UserName == User.Identity.Name).Usuario.Idusuario;
-            
+
             if (userid != car.Idpropietario)
-            {
                 return BadRequest();
-            }
-            
-            
+
             try
             {
                 _context.Carros.Remove(car);
@@ -131,17 +122,112 @@ namespace mobileappAPI.Controllers
             {
                 throw;
             }
-            return Ok( new Response{ Status = "Succeded", Message = "This Car was deleted"});
+            return Ok(new Response { Status = "Succeded", Message = "This Car was deleted" });
 
         }
 
+        [HttpGet("{id}/post")]
+        public async Task<ActionResult<Post>> GetCarPost(int id)
+        {
+            if (MyCarExists(id) && PostExists(id))
+            {
+                return await _context.Posts.SingleAsync(x => x.Idcarro == id);
+            }
+
+            return NotFound(new Response() { Status = "Error", Message = "This car is not registered or hasn't been posted yet" });
+        }
+
+        [HttpPost("{id}/post")]
+        public async Task<ActionResult> PostCarPost(Post post)
+        {
+            if (!MyCarExists(post.Idcarro) || PostExists(post.Idcarro))
+                return NotFound(new Response() { Status = "Error", Message = "This car already has a post or hasn't been registered yet" });
+            
+            try
+            {
+                _context.Posts.Add(post);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                return BadRequest(new Response() { Status = "Error", Message = "This post could not be created, please try again" });
+            }
+            return Ok(new Response { Status = "Succeded", Message = "The post was successfully created" });
+        }
+
+        [HttpPut("{id}/post")]
+        public async Task<IActionResult> UpdatePost(int id, Post post)
+        {
+            if (id != post.Idcarro)
+            {
+                return BadRequest();
+            }
+            _context.Entry(post).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MyCarExists(id) || !PostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(new Response() { Status = "Success", Message = "This car was updated!" });
+        }
+
+
+        [HttpDelete("{id}/post")]
+        public async Task<IActionResult> DeleteCarPost(int id) //Test
+        {
+            if (!MyCarExists(id) || !PostExists(id))
+                return NotFound();
+            
+            var post = await _context.Posts.Include(x => x.IdcarroNavigation).SingleAsync(x => x.Idcarro == id);
+            int userid = _context.Users.Single(u => u.UserName == User.Identity.Name).Usuario.Idusuario;
+
+            if (userid != post.IdcarroNavigation.Idpropietario)
+                return BadRequest();
+
+            try
+            {
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+            return Ok(new Response { Status = "Succeded", Message = "This Car was deleted" });
+
+        }
+
+        [HttpGet("posts")]
+        public async Task<ActionResult<IEnumerable<Post>>> GetMyPosts()
+        {
+            int userid = _context.Users.Single(u => u.UserName == User.Identity.Name).Usuario.Idusuario;
+            var myCars = await _context.Posts.Include(x => x.IdcarroNavigation)
+                .Where(x => x.IdcarroNavigation.Idpropietario == userid).ToListAsync();
+            if (myCars == null)
+            {
+                return NotFound();
+            }
+            return myCars;
+        }
         public bool MyCarExists(int id)
         {
             return _context.Carros.Any(c => c.Idcarro == id);
         }
-        public bool BrandExists(string marca)
+        public bool PostExists(int id)
         {
-            return _context.Marcas.Any(b => b.Marca1.ToLower() == marca.ToLower());
+            return _context.Posts.Any(x => x.Idcarro == id);
         }
     }
 }
---
+++ mobileappAPI/Controllers/ReservationsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mobileappAPI.Authentication;
+using mobileappAPI.Models;
+
+namespace mobileappAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationsController : ControllerBase
+    {
+        private readonly AuthContext _context;
+
+        public ReservationsController(AuthContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reservations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reservacion>>> GetReservacions()
+        {
+            var userid = GetUserId();
+
+            var reservations = await _context.Reservacions.Where(x => x.Idcliente == userid).ToListAsync();
+            if (reservations != null)
+                return reservations;
+
+            return NotFound();
+        }
+
+        [HttpGet("{id}/reservation-dates")]
+        public async Task<ActionResult<IEnumerable<DateTime>>> GetReservationDates(int? id)
+        {
+            if (!CarExists(id))
+            {
+                return NotFound();
+            }
+            List<DateTime> dates = new List<DateTime>();
+            await _context.Reservacions.Where(x => x.Idcarro == id).ForEachAsync(x =>
+            {
+                dates.AddRange(EachCalendarDay(x.FechaSalida,x.FechaEntrega));  
+            });
+
+            return dates;
+        }
+
+        // GET: api/Reservations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reservacion>> GetReservacion(int? id)
+        {
+            var reservacion = await _context.Reservacions.FindAsync(id);
+
+            if (reservacion == null)
+            {
+                return NotFound();
+            }
+
+            return reservacion;
+        }
+
+        // PUT: api/Reservations/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReservacion(int? id, Reservacion reservacion)
+        {
+            if (id != reservacion.Idreservacion)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reservacion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservacionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //POST: api/Reservations
+        //To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Reservacion>> PostReservacion(Reservacion reservacion)
+        {
+            _context.Reservacions.Add(reservacion);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReservacion", new { id = reservacion.Idreservacion }, reservacion);
+        }
+
+        // DELETE: api/Reservations/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Reservacion>> DeleteReservacion(int? id)
+        {
+            var reservacion = await _context.Reservacions.FindAsync(id);
+            if (reservacion == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservacions.Remove(reservacion);
+            await _context.SaveChangesAsync();
+
+            return reservacion;
+        }
+
+
+        private bool ReservacionExists(int? id)
+        {
+            return _context.Reservacions.Any(e => e.Idreservacion == id);
+        }
+
+        public bool CarExists(int? id)
+        {
+            return _context.Carros.Any(x => x.Idcarro == id);
+        }
+
+        public int? GetUserId()
+        {
+            return _context.Users.Single(u => u.UserName == User.Identity.Name).Usuario.Idusuario;
+        }
+
+        public IEnumerable<DateTime> EachCalendarDay(DateTime startDate, DateTime endDate)
+        {
+            for (var date = startDate.Date; date.Date <= endDate.Date; date = date.AddDays(1)) yield
+            return date;
+        }
+    }
+}
